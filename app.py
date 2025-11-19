155b219f64ee85a25c7eb5ee76ea6f9168efcdfd
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-19 09:13:05 +0100

    update name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("jeay, we connecetd everyhting" \
 
 st.button("Click here to start a dinner")
 
-from Feature_01.(Questionnaire) import return_even
+from Feature_01 import return_even
 
 pist=[i for i in range(10)]
 
