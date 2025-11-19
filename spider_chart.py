5b55962ef3a8111be024be55f81dbc9b0d23dc78
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-19 18:19:11 +0100

    f

--- spider_chart.py
+++ spider_chart.py
@@ -6,6 +6,6 @@ def group_taste_profile():
     st.title("This is your groups taste profile of today!")
 
     st.subheader("Let's analyze it.")
-
-st.write(st.session_state["answers"])
+    
+    st.write(st.session_state["answers"])
 
