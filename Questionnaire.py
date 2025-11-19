d9f6e0deb8b5cffa0b3473dcd2c383182d7fa882
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-19 12:40:49 +0100

    p

--- Questionnaire.py
+++ Questionnaire.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.write("This is going to be the questionnaire")
+
+def show_questionnaire():
+        
+        st.title(f"Participant {st.session_state['current_participant']}")
+        
+        
+        if st.button("Next"):
+        # TODO: save answers in session_state
+        # pass
