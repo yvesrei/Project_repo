79d2b6aa251cc96301b1923fff4c4ef543688c64
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-19 14:34:16 +0100

    q

--- Questionnaire.py
+++ Questionnaire.py
@@ -18,13 +18,13 @@ def show_questionnaire():
         type_of_cuisine= st.selectbox(
               "Your cuisine preference",
               options=["italian", "greek", "swiss", "chinese", "thai"],
-              index=None
+              index=None,
               placeholder= "Please choose your prefered type of cuisine"
         )
         dining_sytle= st.selectbox(
               "Your dining style preference",
               options=["takeaway","Casual", "A la carte", "Set Menu / Chef's Menu", "Date Night"],
-              index=None
+              index=None,
               placeholder= "Please choose your prefered dining style"
         )
         if st.button("Next Person"):
