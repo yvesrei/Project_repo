8915861a422e2b6a23a65ffb83a0d202c9327122
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-19 08:45:59 +0100

    trying things out

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ st.write("jeay, we connecetd everyhting" \
          
 "")
 
+st.write("Most objects") # df, err, func, keras!
+st.write(["st", "is <", 3])
+st.write_stream(my_generator)
+st.write_stream(my_llm_stream)
+
+st.text("Fixed width text")
+st.markdown("_Markdown_")
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+st.badge("New")
+st.html("<p>Hi!</p>")
