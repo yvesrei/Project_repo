54db36be46654126fa0472cc2f01381c3e0b15ed
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-20 22:21:33 +0100

    p

--- spider_chart.py
+++ spider_chart.py
@@ -10,7 +10,7 @@ def group_taste_profile(answers):
 
     st.subheader("Let's analyze it.")
 
-    budget_dict:{"$":1, "$$":2, "$$$":3}
+    budget_dict = {"$":1, "$$":2, "$$$":3}
 
     budget_scores= []
     budget_weights=[]
@@ -29,11 +29,45 @@ def group_taste_profile(answers):
 
         budget_weights.append(budget_import)
 
+    group_budget = sum(budget_scores) / sum(budget_weights)
 
+    dining_style_dict= {"Takeaway": 1,"Casual": 2, "A la carte": 3, "Set Menu / Chef's Menu": 4, "Date Night": 5}
 
-    st.write(st.session_state["answers"])
+    dining_style_scores= []
+    dining_style_weights=[]
 
+    for participant in answers:
+        
+        dining_style_value= participant["dining_style"]
 
-def create_spiderchart(values):
-    x=0
+        numeric_dining_style= dining_style_dict[dining_style_value]
+
+        dining_style_import= participant["dining_style_importance"]
+
+        weighted_dining_style_score= numeric_dining_style * dining_style_import
+
+        dining_style_scores.append(weighted_dining_style_score)
+
+        dining_style_weights.append(dining_style_import)
+
+    group_dining_style = sum(dining_style_scores) / sum(dining_style_weights)
+
+    type_of_cuisine_scores = {}
+
+    for participant in answers:
+        importance = participant["type_of_cuisine_importance"]
+        cuisines = participant["type_of_cuisine"]
+
+        for c in cuisines:
+            if c not in type_of_cuisine_scores:
+                type_of_cuisine_scores[c] = 0
+
+            type_of_cuisine_scores[c] += 1 * importance
+
+    return {
+        "budget": group_budget,
+        "dining_style": group_dining_style,
+        "cuisine": type_of_cuisine_scores
+    }
+
 
