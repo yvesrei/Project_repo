3d0f61be5fa30fd8328a289ab449d55e4c3b79ec
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-21 13:28:15 +0100

    budget symbol

--- spider_chart.py
+++ spider_chart.py
@@ -41,6 +41,12 @@ def group_taste_profile(answers):
 
     group_budget = sum(budget_scores) / sum(budget_weights)
 
+    rounded_budget= round(group_budget)
+
+    reverse_budget_dict = {1: "$", 2: "$$", 3: "$$$"}
+
+    budget_symbol_group = reverse_budget_dict.get(rounded_budget, "Unknown")
+
     
     dining_style_scores= []
 
@@ -69,7 +75,7 @@ def group_taste_profile(answers):
 
     
 
-    st.write(f"Your group budget preference is:{group_budget}")
+    st.write(f"Your group budget preference is:{budget_symbol_group}")
 
     st.write (f"Your groups prefers: {type_of_cuisine_scores}")
 
