c252344d9d15f007fd3d09680e98b5e3f4d57481
yvesrei <yves.reichelt@student.unisg.ch>
2025-11-26 13:40:15 +0100

    logo

--- Show_homepage.py
+++ Show_homepage.py
@@ -1,35 +1,63 @@
 import streamlit as st
+import os
 def show_homepage():
         
+        # Path to the logo inside your project
+    logo_path = os.path.join("Images", "FOODMINGLE_Logo_For_Streamlit_App.png")
+
+    # Render centered logo
+    st.markdown(
+        f"""
+        <div style="display:flex; justify-content:center; margin-top:20px;">
+            <img src="{logo_path}" style="width:260px; border-radius:12px;" />
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown(
+        """
+        <h1 style="text-align:center; margin-top:15px; color:#3d3a2a;">
+            Welcome to FoodMingle
+        </h1>
+        <p style="text-align:center; color:#6a6450;">
+            Where tastes meet.
+        </p>
+        """,
+        unsafe_allow_html=True
+    )
+        
+
+        
         ## Shows the title(name of app) and subheader (our slogan)
-        st.title("FOODMINGLE")
-        st.subheader("Where tastes meet!")
+    st.title("FOODMINGLE")
+    st.subheader("Where tastes meet!")
         
         ## The user is able to select how many people will take part in the questionnaire in this select-button
         # The number gets safed in "st.session_state" so the app remembers it across all pages.
-        num_of_part = st.selectbox(
+    num_of_part = st.selectbox(
             "Number of participants",
             options=[2, 3, 4, 5],
             index=None,
             placeholder="Please select a number"
         )
         
-        st.session_state["num_of_participants"] = num_of_part
+    st.session_state["num_of_participants"] = num_of_part
         
         ## This button checks if the user has inserted a valid number of participants.
         # If the user hasn't it will display an Error message
         # If the user has inserted a valdi value, the st.session_state is set to "questionnaire" --> page questionnaire is displayed
-        if st.button("Set up dinner"):
-            if num_of_part is None:
+    if st.button("Set up dinner"):
+        if num_of_part is None:
                 st.warning("Please select a number of participants for your dinner")
                 st.stop
-            else:
+        else:
                 st.session_state["page"] = "questionnaire"
                 st.rerun()
         ## Visual line to seperat the two buttons
-        st.write("---")    
+    st.write("---")    
 
         ## If the user clicks this button, the st.session_state is set to the page "about" --> page about is displayed
-        if st.button("About us", key="about_button"): 
-            st.session_state["page"] = "about"
-            st.rerun()
+    if st.button("About us", key="about_button"): 
+        st.session_state["page"] = "about"
+        st.rerun()
