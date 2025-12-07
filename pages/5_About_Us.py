37886158181ca87d42099d75af23003044775a79
yvesrei <yves.reichelt@student.unisg.ch>
2025-12-07 20:47:50 +0100

    p

--- pages/5_About_Us.py
+++ pages/5_About_Us.py
@@ -7,8 +7,6 @@ init_session()
 
 from About_us import show_about_us
 
-# This is a button to return to the homepage page file
-# We use "switch_page()"" because this page bypasses the router implemented in app.py (st.session_state)
-st.button("⬅️ Back to homepage", on_click=lambda: st.switch_page("pages/1_Homepage.py"))
+
 
 show_about_us()
